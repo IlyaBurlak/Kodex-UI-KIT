53d282356bf56ac14b013d0870015e13d66c8849
ilaburlak <ilya.burlak.burlak@mail.ru>
2025-10-24 14:12:28 +0300

    fetch: add new component
        Добавил новый component checkbox

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+
+import './checkbox.scss';
+
+import type { CheckboxProps } from './Checkbox.types';
+
+export const Checkbox: FC<CheckboxProps> = ({
+  checked,
+  defaultChecked,
+  onChange,
+  disabled = false,
+  size = 'medium',
+  className,
+  label,
+  primary = false,
+  ...props
+}) => {
+  const classList = [
+    'ui-checkbox',
+    `ui-checkbox--${size}`,
+    primary ? 'ui-checkbox--primary' : 'ui-checkbox--secondary',
+    disabled ? 'ui-checkbox--disabled' : '',
+  ].filter(Boolean);
+
+  return (
+    <label className={[...classList, className].filter(Boolean).join(' ')}>
+      <input
+        checked={checked}
+        className="ui-checkbox__input"
+        defaultChecked={defaultChecked}
+        disabled={disabled}
+        type="checkbox"
+        onChange={(e) => onChange && onChange(e.target.checked)}
+        {...props}
+      />
+      <span aria-hidden="true" className="ui-checkbox__box" />
+      {label ? <span className="ui-checkbox__label">{label}</span> : null}
+    </label>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,6 @@ export type { InputProps } from './Input/Input.types';
 
 export { Select } from './Select/Select';
 export type { SelectProps } from './Select/Select.types';
+
+export { Checkbox } from './Checkbox/Checkbox';
+export type { CheckboxProps } from './Checkbox/Checkbox.types';
