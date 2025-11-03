d4ae7e7953fe29551569a753fdeed9415f21182d
ilaburlak <ilya.burlak.burlak@mail.ru>
2025-11-03 13:57:20 +0300

    refactor: исправил PostPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { App } from './App';
-import store from './store';
 import { ThemeProvider } from './Theme/ThemeContext';
 
 import './styles/theme.scss';
+import {store} from "./store";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
